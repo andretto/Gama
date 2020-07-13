210fce530055e6b86a3a6b1d4d53d35cf312d2eb
MarceloAndretto <m.andretto@yahoo.com.br>
2020-07-13 14:49:15 -0300

    Dia Array

---
+++ AULA00061/sample/SampleArray.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class SampleArray {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+
+        lista.add("Marcelo");
+        lista.add("Teste");
+        lista.add("Edna");
+        lista.add("Eduardo");
+        lista.add(2,"Dois");
+        System.out.println(lista);
+
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).charAt(0)=='M' ) {
+                System.out.println("Marcelo Andretto");
+            } else {
+                System.out.println(lista.get(i));
+            }
+        }
+
+        for (String xixi : lista) {
+            
+            
+        }
+
+    }
+    
+}
